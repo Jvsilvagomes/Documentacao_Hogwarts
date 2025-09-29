7394c41c359da3acdb39eedfadd91ca0eee4856b
Jvsilvagomes <joao.silvassenai@gmail.com>
2025-09-29 09:30:43 -0300

    termino do getById

--- src/controllers/bruxosController.js
+++ src/controllers/bruxosController.js
@@ -3,11 +3,37 @@ import dados from "../models/dados.js";
 const { bruxos } = dados;
 let resultado = bruxos;
 
-const getAllBruxos = (req,res) => {
-    res.status(200).json({
-        total: resultado.length,
-        bruxos: resultado
-    })
-}
+const getAllBruxos = (req, res) => {
+  res.status(200).json({
+    total: resultado.length,
+    bruxos: resultado,
+  });
+};
 
-export { getAllBruxos };
+const getBruxosById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const bruxo = bruxos.find((b) => b.id === parseInt(id));
+
+  if (!bruxo) {
+    res.status(404).json({
+      status: 404,
+      success: false,
+      message: "Bruxo not found in Hogwarts registry",
+      error: "BRUXO_NOT_FOUND",
+      suggestions: [
+        "Check the wizard name spelling",
+        "Verify if the wizard is registered",
+      ],
+    });
+  }
+
+  res.status(200).json({
+    status: 200,
+    success: true,
+    total: bruxo.length,
+    bruxo: bruxo,
+  });
+};
+
+export { getAllBruxos, getBruxosById };
