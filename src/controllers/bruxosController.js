de85cda567738e35f06b910e9d26a1041b899b5d
Jvsilvagomes <joao.silvassenai@gmail.com>
2025-09-29 10:27:44 -0300

    termino do deleteBruxo

--- src/controllers/bruxosController.js
+++ src/controllers/bruxosController.js
@@ -60,18 +60,60 @@ const createBruxo = (req, res) => {
     anoNascimento: parseInt(anoNascimento),
     especialidade,
     nivelmagia,
-    ativo
-  }
+    ativo,
+  };
 
   bruxos.push(novoBruxo);
 
   res.status(201).json({
-    status:200,
+    status: 200,
     success: true,
     message: "Bruxo criado com sucesso",
     bruxo: novoBruxo,
-    total: novoBruxo.length
-  })
+    total: novoBruxo.length,
+  });
 };
 
-export { getAllBruxos, getBruxosById, createBruxo};
+const deleteBruxo = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if (isNaN(id)) {
+    return res.status(400).json({
+      status: 400,
+      success: false,
+      message: "ID inválido. O ID deve ser um número.",
+      error: "INVALID_ID",
+      details: "O ID fornecido não é um número válido.",
+    });
+  }
+
+  const bruxosParaRemover = bruxos.filter((b) => b.id === id);
+
+  if (!bruxosParaRemover.length) {
+    return res.status(404).json({
+      status: 404,
+      success: false,
+      message: "Bruxo não REGISTRADO!",
+      error: "BRUXO_NOT_FOUND",
+      suggestions: [
+        "Verifique a grafia do nome do bruxo",
+        "Verifique se o bruxo está registrado",
+      ],
+    });
+  }
+
+  const bruxosFiltrados = bruxos.filter((b) => b.id !== id);
+
+  bruxos.splice(0, bruxos.length, ...bruxosFiltrados);
+
+  res.status(200).json({
+    status: 200,
+    success: true,
+    message: `Bruxo ${id} removido com sucesso!!`,
+    bruxo: bruxosParaRemover,
+    total: bruxosParaRemover.length,
+  });
+
+};
+
+export { getAllBruxos, getBruxosById, createBruxo, deleteBruxo };
--- src/routes/bruxosRoutes.js
+++ src/routes/bruxosRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getAllBruxos, getBruxosById, createBruxo} from "../controllers/bruxosController.js";
+import { getAllBruxos, getBruxosById, createBruxo, deleteBruxo} from "../controllers/bruxosController.js";
 
 const router = express.Router();
 
 router.get("/", getAllBruxos);
 router.get("/:id", getBruxosById);
 router.post("/", createBruxo);
+router.delete("/:id", deleteBruxo);
 
 export default router;
